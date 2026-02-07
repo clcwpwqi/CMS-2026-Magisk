435e0fc2f91fddb9fba376ffbcf09cf50ccc7508
clcwpwqi <102562342+clcwpwqi@users.noreply.github.com>
2026-02-07 08:46:57 +0800

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,10 @@
+SKIPUNZIP=0
+
+
+ui_print "*******************************"
+ui_print "   CMS-2026 Encryption Module  "
+ui_print "   GitHub @clcwpwqi            "
+ui_print "*******************************"
+ui_print "- Installing WebUI components..."
+ui_print "- Installation Complete!"
+ui_print "*******************************"
